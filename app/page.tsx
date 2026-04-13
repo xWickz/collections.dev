a734ca4c99cee36c3299f2b7a9be5ad8c92def7b
xWickz <wickzcs@gmail.com>
2026-04-13 15:04:36 -0400

    Agregar Suspense alrededor del componente de búsqueda para mejorar la experiencia de carga

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Hero from "@/components/Hero";
 import LatestArticles from "@/components/LatestArticles";
 import Search from "@/components/Search";
@@ -10,7 +11,11 @@ export default async function HomePage({
   return (
     <main className="min-h-screen py-12 px-12 bg-void-eclipse">
       <Hero />
-      <Search placeholder="Buscar artículos..." />
+      <Suspense
+        fallback={<p className="text-zinc-500 mt-8">Cargando búsqueda...</p>}
+      >
+        <Search placeholder="Buscar artículos..." />
+      </Suspense>
       <LatestArticles searchParams={searchParams} />
     </main>
   );
