9d233fce76fe2fea3714073911340358d9b7ac05
xWickz <wickzcs@gmail.com>
2026-04-13 16:47:01 -0400

    Agregar componente de búsqueda y explicación en la documentación

--- components/search.tsx
+++ components/search.tsx
@@ -32,6 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
           fill="#6B7280"
           className="shrink-0"
         >
+          <title>Search Icon</title>
           <path d="M13 3C7.489 3 3 7.489 3 13s4.489 10 10 10a9.95 9.95 0 0 0 6.322-2.264l5.971 5.971a1 1 0 1 0 1.414-1.414l-5.97-5.97A9.95 9.95 0 0 0 23 13c0-5.511-4.489-10-10-10m0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8" />
         </svg>
         <input
